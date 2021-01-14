707b2d868e0a1ad20f8123de33fe20bd97f335af
KHardik1698 <hardik.kundalia@contentstack.com>
2021-01-14 23:39:58 +0530

    Added Styling to Not Found Component
    Added Responsive Styling to Not Found Component

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,21 +1,20 @@
 import { Component } from "react";
 import { Link } from "react-router-dom";
-// import styles from "../styles/NotFound.module.css";
+import styles from "./index.module.css";
 
 class NotFound extends Component {
   render() {
     return (
-      <h1>Not Found Component</h1>
-      // <div className={styles["not-found-container"]}>
-      //   <img
-      //     className={styles["not-found-image"]}
-      //     src="https://image.freepik.com/free-vector/404-error-page-found_41910-364.jpg"
-      //     alt="Page Not Found"
-      //   />
-      //   <Link className={styles["back-button"]} to="/">
-      //     <button>Go to Home</button>
-      //   </Link>
-      // </div>
+      <div className={styles["not-found-container"]}>
+        <img
+          className={styles["not-found-image"]}
+          src="https://image.freepik.com/free-vector/404-error-page-found_41910-364.jpg"
+          alt="Page Not Found"
+        />
+        <Link className={styles["back-button"]} to="/">
+          <button>Go Back</button>
+        </Link>
+      </div>
     );
   }
 }
