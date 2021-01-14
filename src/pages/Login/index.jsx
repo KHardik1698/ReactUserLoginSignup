7d59d80b43022a68d7224e764b59226693f10c2c
KHardik1698 <hardik.kundalia@contentstack.com>
2021-01-14 15:26:10 +0530

    Created a Basic Login Form
    Login Form consists of Email & Password

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,10 +1,30 @@
 import { Component } from "react";
 
 class Login extends Component {
+  state = {
+    loggedIn: false,
+  };
+  loginUser = (event) => {
+    event.preventDefault();
+  };
   render() {
     return (
       <div>
         <h1>Login Page</h1>
+        <form onSubmit={this.loginUser}>
+          <div>
+            <input type="text" name="email" placeholder="Enter Email" required />
+            <input type="password" name="password" placeholder="Enter Password" required />
+          </div>
+          <div>
+            <button type="submit" value="login">
+              Login
+            </button>
+            <button type="reset" value="reset">
+              Clear
+            </button>
+          </div>
+        </form>
       </div>
     );
   }
