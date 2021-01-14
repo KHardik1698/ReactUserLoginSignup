2773433c0bfe190156b12320d2c7dc41dc82ff5e
KHardik1698 <hardik.kundalia@contentstack.com>
2021-01-14 14:52:37 +0530

    Created a Signup Component
    Added basic Header to display Signup Page

---
+++ src/pages/signup/index.jsx
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class Signup extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Signup Page</h1>
+      </div>
+    );
+  }
+}
+
+export default Signup;
--- src/routers/router.js
+++ src/routers/router.js
@@ -2,6 +2,7 @@ import { Component } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import App from "../App";
 import Login from "../pages/login";
+import Signup from "../pages/signup";
 
 class Router extends Component {
   render() {
@@ -11,6 +12,7 @@ class Router extends Component {
           <Switch>
             <Route path="/" exact component={App} />
             <Route path="/login" exact component={Login} />
+            <Route path="/signup" exact component={Signup} />
           </Switch>
         </BrowserRouter>
       </div>
