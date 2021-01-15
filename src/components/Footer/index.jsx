2f02a8bf0a721174009682af8e717df0e6d009a8
KHardik1698 <hardik.kundalia@contentstack.com>
2021-01-15 23:46:13 +0530

    Updated Header & Footer Components Styling
    The Color Theme of Header & Footer has been updated

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -12,11 +12,11 @@ function Footer() {
     <div className={styles["footer-container"]}>
       <h1 className={styles["copyright"]}>Developed by Hardik &copy; RE/CS</h1>
       <div className={styles["icons-container"]}>
-        <FaFacebookSquare className={styles["icon"]} size="7vh" color="#143cdc" />
-        <FaTwitterSquare className={styles["icon"]} size="7vh" color="#10b9bf" />
-        <FaInstagramSquare className={styles["icon"]} size="7vh" color="#dd29dd" />
-        <FaGithubSquare className={styles["icon"]} size="7vh" color="#00040c" />
-        <FaLinkedin className={styles["icon"]} size="7vh" color="#1f6dc0" />
+        <FaFacebookSquare className={styles["icon"]} size="7vh" color="black" />
+        <FaTwitterSquare className={styles["icon"]} size="7vh" color="black" />
+        <FaInstagramSquare className={styles["icon"]} size="7vh" color="black" />
+        <FaGithubSquare className={styles["icon"]} size="7vh" color="black" />
+        <FaLinkedin className={styles["icon"]} size="7vh" color="black" />
       </div>
     </div>
   );
